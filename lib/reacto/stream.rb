1e69eb6d2553779327905bd74afdebcb4ede0ee4
meddle <meddlesmerch@gmail.com>
2015-10-15 00:08:56 +0300

    Notify logic idea.

--- lib/reacto/stream.rb
+++ lib/reacto/stream.rb
@@ -3,11 +3,20 @@ module Reacto
   class Stream
 
     def on(type = :value, listener)
+      listeners(type) << listener
+    end
+
+    private
+
+    def listeners(type = :value)
       @listeners ||= {}
       @listeners[type] ||= []
 
-      @listeners[type] << listener
+      @listeners[type]
     end
 
+    def notify(type = :value)
+      listeners(type).each(&:call)
+    end
   end
 end
