aaebbee44d16ac19afef3f8334394d0945caea74
meddle <meddlesmerch@gmail.com>
2015-10-19 00:03:45 +0300

    Subscription?

--- lib/reacto/stream.rb
+++ lib/reacto/stream.rb
@@ -1,6 +1,5 @@
 module Reacto
   class Stream # Trackable?
-
     def on(listener, type: :value)
       listeners(type) << listener
     end
@@ -18,4 +17,27 @@ module Reacto
       listeners(type).each(&:call)
     end
   end
+
+  class Subscription
+    attr_reader :notification_tracker, :trackable, :type
+
+    def initialize(notification_tracker, trackable, type)
+      @notification_tracker = notification_tracker
+      @trackable = trackable
+      @type = type
+    end
+
+    def subscribed?
+      !trackable.nil? && trackable.subscribed?(notification_tracker, type)
+    end
+
+    def unsubscribe
+      @trackable.off(notification_tracker)
+
+      @trackable = nil
+      @notification_tracker = nil
+      @type = nil
+    end
+  end
+
 end
