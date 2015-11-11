253e167371887877eee03394faf0d860c72f22c0
meddle <meddlesmerch@gmail.com>
2015-11-12 00:18:12 +0200

    First working thing.

--- spec/reacto/trackable_spec.rb
+++ spec/reacto/trackable_spec.rb
@@ -12,6 +12,18 @@ describe Reacto::Trackable do
       expect(actual).to_not be(nil)
       expect(actual).to be_kind_of(Reacto::Subscription)
     end
-  end
 
+    context('value') do
+      it 'the trackable behavior uses a subscription which `on_value` is the passed value action' do
+        test_data = []
+        behaviour = -> (s) do
+          s.on_value(5)
+        end
+        described_class.new(behaviour).on(value: -> (v) { test_data << v })
+
+        expect(test_data.size).to be(1)
+        expect(test_data[0]).to be(5)
+      end
+    end
+  end
 end
