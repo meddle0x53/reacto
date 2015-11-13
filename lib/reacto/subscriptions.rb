56e19f58e839ef77b7c458a0984546caee98e9fe
meddle <meddlesmerch@gmail.com>
2015-11-13 15:52:57 +0200

    Initial implementation of an operator.

--- lib/reacto/subscriptions.rb
+++ lib/reacto/subscriptions.rb
@@ -1,6 +1,7 @@
 require 'reacto/subscriptions/subscription'
 require 'reacto/subscriptions/subscription_wrapper'
 require 'reacto/subscriptions/tracker_subscription'
+require 'reacto/subscriptions/operation_subscription'
 
 module Reacto
   module Subscriptions
---
+++ lib/reacto/subscriptions/operation_subscription.rb
@@ -0,0 +1,46 @@
+require 'reacto/subscriptions/subscription'
+require 'reacto/tracker'
+
+module Reacto
+  module Subscriptions
+    class OperationSubscription < Reacto::Tracker
+      extend Forwardable
+      include Subscription
+
+      delegate ['subscribed?', :unsubscribe] => :@wrapped
+
+      def initialize(
+        subscription,
+        open: NO_ACTION,
+        value: NO_ACTION,
+        error: DEFAULT_ON_ERROR,
+        close: NO_ACTION
+      )
+        super(open: open, value: value, error: error, close: close)
+
+        @wrapped = subscription
+      end
+
+      def open(&block)
+        @open = block
+        self
+      end
+
+      def value(&block)
+        @value = block
+        self
+      end
+
+      def error(&block)
+        @error = block
+        self
+      end
+
+      def close(&block)
+        @close = block
+        self
+      end
+
+    end
+  end
+end
