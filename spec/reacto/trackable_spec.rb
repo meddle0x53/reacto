123a092154c1b9202acda6407fdcd63320df172a
meddle <meddlesmerch@gmail.com>
2015-11-15 00:26:41 +0200

    Spect for the current logic.

--- spec/reacto/trackable_spec.rb
+++ spec/reacto/trackable_spec.rb
@@ -4,6 +4,13 @@ describe Reacto::Trackable do
   let(:test_data) { [] }
   let(:test_on_value) { -> (v) { test_data << v }}
 
+  let(:test_behaviour) do
+    lambda do |tracker_subscription|
+      tracker_subscription.on_value(5)
+      tracker_subscription.on_close
+    end
+  end
+
   describe '.new' do
     it 'supports behavior invoked on tracking, passed as block' do
       trackable = described_class.new do |tracker_subscription|
@@ -27,14 +34,42 @@ describe Reacto::Trackable do
 
     context('value') do
       it 'the trackable behavior uses a subscription which `on_value` is the passed value action' do
-        behaviour = -> (s) do
-          s.on_value(5)
-        end
-        described_class.new(behaviour).on(value: test_on_value)
+        described_class.new(test_behaviour).on(value: test_on_value)
 
         expect(test_data.size).to be(1)
         expect(test_data[0]).to be(5)
       end
+    end
+  end
+
+  describe '#lift' do
+    it 'applies a transformation to the trackable behaviour' do
+      trackable = described_class.new(test_behaviour)
+      lifted_trackable = trackable.lift do |tracker_subscription|
+        Reacto::Subscriptions::OperationSubscription.new(
+          tracker_subscription,
+          value: -> (v) { tracker_subscription.on_value(v * v) }
+        )
+      end
+
+      lifted_trackable.on(value: test_on_value)
+
+      expect(test_data.size).to be(1)
+      expect(test_data[0]).to be(25)
+    end
+  end
+
+  describe '#map' do
+    it 'transforms the value of the source Trackable using the passed transformation' do
+      source = described_class.new(test_behaviour)
+      trackable = source.map do |v|
+        v * v * v
+      end
+
+      trackable.on(value: test_on_value)
+
+      expect(test_data.size).to be(1)
+      expect(test_data[0]).to be(125)
     end
   end
 end
