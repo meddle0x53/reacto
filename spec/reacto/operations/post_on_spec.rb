39409767f193bc44897cc80a5a1dfef7a958750e
meddle <meddlesmerch@gmail.com>
2015-11-16 12:02:32 +0200

    PostOn spec case.

--- spec/reacto/operations/post_on_spec.rb
+++ spec/reacto/operations/post_on_spec.rb
@@ -1,8 +1,25 @@
 require 'spec_helper'
 
 require 'reacto/operations/post_on'
+require 'reacto/subscriptions/executor_subscription'
+require 'reacto/executors'
+require 'reacto/tracker'
 
 describe Reacto::Operations::PostOn do
+
+  subject do
+    described_class.new(Reacto::Executors.immediate)
+  end
+
+  context '#call' do
+    it 'returns a special ExecutorSubscription' do
+      subscription = subject.call(Reacto::Tracker.new)
+
+      expect(subscription).to(
+        be_instance_of(Reacto::Subscriptions::ExecutorSubscription)
+      )
+    end
+  end
 
 end
 
