366c7ea9b27abbd6e995db5d80ff982c7d58f233
meddle <meddlesmerch@gmail.com>
2015-12-09 12:55:45 +0200

    Prepend & Interval specs.

--- spec/reacto/create_trackable_spec.rb
+++ spec/reacto/create_trackable_spec.rb
@@ -117,9 +117,41 @@ context Reacto::Trackable do
     end
 
     it 'it can use a specific executor' do
-      trackable = described_class.later(0.2, 5, Reacto::Executors.immediate)
+      trackable = described_class.later(
+        0.2, 5, executor: Reacto::Executors.immediate
+      )
       subscription = attach_test_trackers(trackable)
       expect(test_data).to be == [5, '|']
+    end
+  end
+
+  context '.interval' do
+    it 'emits an infinite sequence of number on every n seconds by default' do
+      trackable = described_class.interval(0.1)
+      subscription = attach_test_trackers(trackable)
+      sleep 1
+      subscription.unsubscribe
+
+      expect(test_data).to be == (0..8).to_a
+    end
+
+    it 'can use any enumerator to produce the sequence to emit' do
+      trackable = described_class.interval(0.1, ('a'..'z').each)
+      subscription = attach_test_trackers(trackable)
+      sleep 1
+      subscription.unsubscribe
+
+      expect(test_data).to be == ('a'..'i').to_a
+    end
+
+    it 'can use the immediate executor to block the current thread while ' \
+      'emitting' do
+      trackable = described_class.interval(
+        0.1, (1..5).each, executor: Reacto::Executors.immediate
+      )
+      subscription = attach_test_trackers(trackable)
+
+      expect(test_data).to be == (1..5).to_a + ['|']
     end
   end
 
--- spec/reacto/trackable_spec.rb
+++ spec/reacto/trackable_spec.rb
@@ -154,6 +154,18 @@ context Reacto::Trackable do
     end
   end
 
+  context 'prepend' do
+    it 'emits the passed enumerable before the values, emited by the caller' do
+      source = described_class.enumerable((1..5))
+      trackable = source.prepend((-5..0))
+
+      trackable.on(value: test_on_value)
+
+      expect(test_data.size).to be(11)
+        expect(test_data).to be == (-5..5).to_a
+    end
+  end
+
   context '#drop & #take' do
     let(:test_behaviour) do
       lambda do |tracker_subscription|
