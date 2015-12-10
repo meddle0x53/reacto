8b7513d6474dc787a22f43698ae34cc3e9677ff0
meddle <meddlesmerch@gmail.com>
2015-12-11 00:04:22 +0200

    Better take operator.

--- lib/reacto/operations/take.rb
+++ lib/reacto/operations/take.rb
@@ -3,7 +3,6 @@ require 'reacto/subscriptions/operation_subscription'
 module Reacto
   module Operations
     class Take
-
       def initialize(how_many_to_take)
         if how_many_to_take < 0
           raise ArgumentError.new('Attempt to take negative size!')
@@ -11,14 +10,17 @@ module Reacto
 
         @how_many_to_take = how_many_to_take
         @taken = 0
+        @closed = false
       end
 
       def call(tracker)
         behaviour = lambda do |value|
+          return if @closed
           if @taken < @how_many_to_take
             tracker.on_value(value)
             @taken += 1
           else
+            @closed = true
             tracker.on_close
           end
         end
