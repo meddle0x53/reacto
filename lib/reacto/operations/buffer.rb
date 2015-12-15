93f613f301a2f46e9dc2b023f480bf714e1829d4
meddle <meddlesmerch@gmail.com>
2015-12-16 00:55:28 +0200

    Buffer operator.

---
+++ lib/reacto/operations/buffer.rb
@@ -0,0 +1,45 @@
+require 'reacto/subscriptions/operation_subscription'
+
+module Reacto
+  module Operations
+    class Buffer
+      def initialize(count: nil, delay: nil)
+        @count = count
+        @delay = delay
+        @buffer = []
+      end
+
+      def call(tracker)
+        close = lambda do
+          tracker.on_value(@buffer) unless @buffer.empty?
+          tracker.on_close
+        end
+        value = if !count.nil? && delay.nil?
+                  count_buffer_behaviour(tracker)
+                else
+                  tracker.method(:on_value)
+                end
+
+        Subscriptions::OperationSubscription.new(
+          tracker,
+          value: value,
+          close: close
+        )
+      end
+
+      private
+
+      def count_buffer_behaviour(tracker)
+        lambda do |value|
+          @buffer << value
+
+          if @buffer.size >= count
+            tracker.on_value(@buffer)
+            @buffer = []
+          end
+        end
+      end
+    end
+  end
+end
+
