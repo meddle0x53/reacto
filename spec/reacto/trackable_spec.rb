33f6a7a95d28233fec4a5cba701efc3dcd6681fe
meddle <meddlesmerch@gmail.com>
2015-12-23 01:48:20 +0200

    Buffer with delay spec.

--- spec/reacto/trackable_spec.rb
+++ spec/reacto/trackable_spec.rb
@@ -307,7 +307,7 @@ context Reacto::Trackable do
 
     context '#buffer' do
       context 'count' do
-        it 'sends value on batches with the size of the passed count' do
+        it 'sends values on batches with the size of the passed count' do
           trackable = described_class.enumerable((1..20)).buffer(count: 5)
           trackable.on(
             value: test_on_value, close: test_on_close, error: test_on_error
@@ -315,6 +315,25 @@ context Reacto::Trackable do
 
           expect(test_data).to be ==
             [(1..5).to_a, (6..10).to_a, (11..15).to_a, (16..20).to_a, '|']
+        end
+      end
+
+      context 'delay' do
+        it 'sends values on batches on intervals - the passed delay' do
+          trackable = described_class.interval(0.1).take(20).buffer(delay: 0.5)
+          subscription = trackable.on(
+            value: test_on_value, close: test_on_close, error: test_on_error
+          )
+          trackable.await(subscription)
+          expect(test_data).to be ==
+            [
+              [0, 1, 2, 3],
+              [4, 5, 6, 7, 8],
+              [9, 10, 11, 12, 13],
+              [14, 15, 16, 17, 18],
+              [19],
+              "|"
+          ]
         end
       end
     end
