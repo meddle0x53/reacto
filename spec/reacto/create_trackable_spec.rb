d5220158e182fae35da7dbfbd20ce205c31f1570
meddle <meddlesmerch@gmail.com>
2015-12-24 00:00:59 +0200

    All current specs.

--- spec/reacto/create_trackable_spec.rb
+++ spec/reacto/create_trackable_spec.rb
@@ -23,6 +23,15 @@ context Reacto::Trackable do
     end
   end
 
+  context '.error' do
+    it 'creates a new trackable emitting only the error passed' do
+      err = StandardError.new('Errrr')
+      attach_test_trackers described_class.error(err)
+
+      expect(test_data).to be == [err]
+    end
+  end
+
   context '.make' do
     it 'creates a new trackable with custom behaviour passed as lambda' do
       behaviour = lambda do |tracker|
