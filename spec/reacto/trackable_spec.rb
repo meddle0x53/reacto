2afa36e1128b14654e53263d0ada36c125e7a276
meddle <meddlesmerch@gmail.com>
2015-12-28 02:01:03 +0200

    Uniq spec.

--- spec/reacto/trackable_spec.rb
+++ spec/reacto/trackable_spec.rb
@@ -357,6 +357,18 @@ context Reacto::Trackable do
             [[1, 2, 3], [4, 5], [6], '|']
         end
       end
+
+      context 'uniq' do
+        it 'sends only uniq values, dropping the repeating ones' do
+          trackable =
+            described_class.enumerable([1, 2, 3, 2, 4, 3, 2, 1, 5]).uniq
+
+          trackable.on(
+            value: test_on_value, close: test_on_close, error: test_on_error
+          )
+          expect(test_data).to be == [1, 2, 3, 4, 5, '|']
+        end
+      end
     end
   end
 end
