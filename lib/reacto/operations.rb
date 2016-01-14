5111a6c5123c35e2db929ee0b6300df540c92cc7
meddle <meddlesmerch@gmail.com>
2016-01-15 00:18:21 +0200

    Diff operation included to the rest of the operations.

--- lib/reacto/operations.rb
+++ lib/reacto/operations.rb
@@ -1,6 +1,7 @@
 require 'reacto/operations/map'
 require 'reacto/operations/select'
 require 'reacto/operations/inject'
+require 'reacto/operations/diff'
 require 'reacto/operations/drop'
 require 'reacto/operations/take'
 require 'reacto/operations/last'
