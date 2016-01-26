8ad930394cac6fd08b285b243f83d2ed208baf82
meddle <meddlesmerch@gmail.com>
2016-01-26 09:54:16 +0200

    InnerSubscription in its own file.

--- lib/reacto/subscriptions/combining_subscription.rb
+++ lib/reacto/subscriptions/combining_subscription.rb
@@ -1,54 +1,11 @@
 require 'reacto/constants'
 require 'reacto/subscriptions/subscription'
+require 'reacto/subscriptions/inner_subscription'
 
 module Reacto
   module Subscriptions
     class CombiningSubscription
       include Subscription
-
-      class InnerSubscription < SimpleSubscription
-        include Subscription
-
-        attr_reader :last_value, :last_error
-
-        def initialize(parent)
-          @parent = parent
-          @closed = false
-          @active = false
-          @last_value = NO_VALUE
-          @last_error = nil
-
-          open = lambda do
-            @active = true
-            @parent.on_open
-          end
-
-          value = lambda do |v|
-            @last_value = v
-            @parent.on_value(v)
-          end
-
-          error = lambda do |e|
-            @last_error = e
-            @parent.on_error(e)
-          end
-
-          close = lambda do
-            @closed = true
-            @parent.on_close
-          end
-
-          super(open: open, value: value, error: error, close: close)
-        end
-
-        def active?
-          @active
-        end
-
-        def closed?
-          @closed
-        end
-      end
 
       def initialize(combinator, subscriber)
         @combinator = combinator
---
+++ lib/reacto/subscriptions/inner_subscription.rb
@@ -0,0 +1,47 @@
+module Reacto
+  module Subscriptions
+    class InnerSubscription < SimpleSubscription
+      include Subscription
+
+      attr_reader :last_value, :last_error
+
+      def initialize(parent)
+        @parent = parent
+        @closed = false
+        @active = false
+        @last_value = NO_VALUE
+        @last_error = nil
+
+        open = lambda do
+          @active = true
+          @parent.on_open
+        end
+
+        value = lambda do |v|
+          @last_value = v
+          @parent.on_value(v)
+        end
+
+        error = lambda do |e|
+          @last_error = e
+          @parent.on_error(e)
+        end
+
+        close = lambda do
+          @closed = true
+          @parent.on_close
+        end
+
+        super(open: open, value: value, error: error, close: close)
+      end
+
+      def active?
+        @active
+      end
+
+      def closed?
+        @closed
+      end
+    end
+  end
+end
