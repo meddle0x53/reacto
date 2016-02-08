4e09f5234ff273143aa89d51f6ffebfa1fdf4d2e
meddle <meddlesmerch@gmail.com>
2016-02-09 00:08:22 +0200

    Drop operator has offset.

--- lib/reacto/operations/drop.rb
+++ lib/reacto/operations/drop.rb
@@ -1,22 +1,35 @@
+require 'reacto/constants'
 require 'reacto/subscriptions/operation_subscription'
 
 module Reacto
   module Operations
     class Drop
 
-      def initialize(how_many_to_drop)
+      def initialize(how_many_to_drop, offset = NO_VALUE)
         if how_many_to_drop < 0
           raise ArgumentError.new('Attempt to drop negative size!')
         end
 
         @how_many_to_drop = how_many_to_drop
         @dropped = 0
+        @offset = offset
       end
 
       def call(tracker)
         behaviour = lambda do |value|
           @dropped += 1
-          tracker.on_value(value) if @dropped > @how_many_to_drop
+
+          if @dropped > @how_many_to_drop
+            if @offset != NO_VALUE
+              if @offset <= 0
+                tracker.on_close
+                return
+              else
+                @offset -= 1
+              end
+            end
+            tracker.on_value(value)
+          end
         end
 
         Subscriptions::OperationSubscription.new(
