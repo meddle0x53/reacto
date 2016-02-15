2e5d42231e1ed720ad3d3a766525b6c8036b5a52
meddle <meddlesmerch@gmail.com>
2016-02-16 00:07:48 +0200

    An idea for a delay.

---
+++ lib/reacto/operations/delay.rb
@@ -0,0 +1,26 @@
+require 'reacto/constants'
+require 'reacto/subscriptions/operation_subscription'
+require 'reacto/executors'
+
+module Reacto
+  module Operations
+    class Delay
+
+      def initialize(timeout, executor = Concurrent::ImmediateExecutor)
+        @timeout = timeout
+        @executor = executor
+      end
+
+      def call(tracker)
+        return tracker if @timeout <= 0
+
+
+        # TODO
+        Subscriptions::OperationSubscription.new(
+          tracker
+        )
+      end
+    end
+  end
+end
+
