db8c92c36243c03974346b1f8aa23df41b692633
meddle <meddlesmerch@gmail.com>
2016-02-20 02:13:18 +0200

    Drop errors operation.

---
+++ lib/reacto/operations/drop_errors.rb
@@ -0,0 +1,17 @@
+require 'reacto/subscriptions/operation_subscription'
+
+module Reacto
+  module Operations
+    class DropErrors
+
+      def call(tracker)
+
+        Subscriptions::OperationSubscription.new(
+          tracker,
+          error: ->(e) {}
+        )
+      end
+    end
+  end
+end
+
