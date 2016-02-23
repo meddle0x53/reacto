9bf79a25eec21a393172eba80daf0a6027504271
meddle <meddlesmerch@gmail.com>
2016-02-23 07:42:57 +0200

    drop_errors spec.

--- spec/reacto/trackable_spec.rb
+++ spec/reacto/trackable_spec.rb
@@ -230,6 +230,18 @@ context Reacto::Trackable do
     end
   end
 
+  context '#drop_errors' do
+    it 'drops all the errors from the source and continues' do
+      described_class.enumerable((1..5)).concat(
+        described_class.error(StandardError.new)
+      ).concat(described_class.enumerable(6..10)).drop_errors.on(
+        value: test_on_value, error: test_on_error, close: test_on_close
+      )
+
+      expect(test_data).to be == (1..10).to_a + ['|']
+    end
+  end
+
   context '#drop, #take, #last, #first, #[]' do
     let(:test_behaviour) do
       lambda do |tracker_subscription|
