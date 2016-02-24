5ce2a8d28e85c6827b4e89193d86a3681d83b472
meddle <meddlesmerch@gmail.com>
2016-02-24 09:27:11 +0200

    Gap fixes.

--- lib/reacto/operations/concat.rb
+++ lib/reacto/operations/concat.rb
@@ -3,6 +3,7 @@ require 'reacto/subscriptions/operation_subscription'
 module Reacto
   module Operations
     class Concat
+      # TODO Continue on error flag!
       def initialize(trackable)
         @trackable = trackable
       end
@@ -10,7 +11,11 @@ module Reacto
       def call(tracker)
         Subscriptions::OperationSubscription.new(
           tracker,
-          close: -> () { @trackable.send(:do_track, tracker) }
+          close: -> () { @trackable.send(:do_track, tracker) },
+          error: -> (e) { 
+            tracker.on_error(e)
+            @trackable.send(:do_track, tracker)
+          }
         )
       end
     end
