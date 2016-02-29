f613bcf61e8fcfa0477a55ffbedd3108636eebed
meddle <meddlesmerch@gmail.com>
2016-02-29 07:18:59 +0200

    Remove unused require.

--- lib/reacto/subscriptions/combining_subscription.rb
+++ lib/reacto/subscriptions/combining_subscription.rb
@@ -1,6 +1,3 @@
-require 'reacto/constants'
-require 'reacto/subscriptions/subscription'
-require 'reacto/subscriptions/inner_subscription'
 require 'reacto/subscriptions/composite_subscription'
 
 module Reacto
