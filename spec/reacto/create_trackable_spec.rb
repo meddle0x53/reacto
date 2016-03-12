97a40ec207603bb9e382e20782d0bc59a60790ea
meddle <meddlesmerch@gmail.com>
2016-03-12 09:57:53 +0200

    Reacto create_trackable_spec uses new support modules.

--- spec/reacto/create_trackable_spec.rb
+++ spec/reacto/create_trackable_spec.rb
@@ -2,19 +2,6 @@ require 'spec_helper'
 
 context Reacto::Trackable do
 
-  let(:test_data) { [] }
-  let(:test_on_value) { -> (v) { test_data << v }}
-  let(:test_on_close) { -> () { test_data << '|' }}
-  let(:test_on_error) { -> (e) { test_data << e }}
-
-  def attach_test_trackers(trackable)
-    trackable.on(
-      value: test_on_value,
-      error: test_on_error,
-      close: test_on_close
-    )
-  end
-
   context '.never' do
     it 'creates a new trackable, which won\'t send any notifications' do
       attach_test_trackers described_class.never
