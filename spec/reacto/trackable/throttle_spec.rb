c7385565b9605739935b0cb2e59740dea78ac45c
meddle <meddlesmerch@gmail.com>
2016-03-22 10:32:57 +0200

    throttle spec.

---
+++ spec/reacto/trackable/throttle_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+context Reacto::Trackable do
+  context '#throttle' do
+    it 'emits only the last values received after a given timeout' do
+      trackable = described_class.interval(0.1).take(30).throttle(0.5)
+      subscription = trackable.on(
+        value: test_on_value, close: test_on_close, error: test_on_error
+      )
+      trackable.await(subscription)
+      expect(test_data).to be == [4, 9, 14, 19, 24, 29, "|"]
+    end
+  end
+end
+
