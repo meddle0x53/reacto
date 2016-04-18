6b94e465075e245d57d14a84132f37308be14662
meddle <meddlesmerch@gmail.com>
2016-04-19 00:02:16 +0300

    Some class-level specs.

---
+++ spec/reacto/trackable/class_level/enumerable_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+
+context Reacto::Trackable do
+  context '.enumerable' do
+    it 'emits the whole enumerable one-by-one and then closes' do
+      trackable = described_class.enumerable([1, 3, 5, 6, 7, 8])
+      subscription = attach_test_trackers(trackable)
+
+      expect(test_data).to be == [1, 3, 5, 6, 7, 8, '|']
+    end
+
+    it 'on error it emits all til the error and the error' do
+      class PositiveArray
+        include Enumerable
+
+        def error
+          @error ||= StandardError.new('Bad.')
+        end
+
+        def initialize(*members)
+          @members = members
+        end
+
+        def each(&block)
+          @members.each do |member|
+            raise error if member < 0
+            block.call(member)
+          end
+        end
+      end
+
+      enumerable = PositiveArray.new(2, 3, -4, 3, 6)
+      trackable = described_class.enumerable(enumerable)
+      subscription = attach_test_trackers(trackable)
+
+      expect(test_data).to be == [2, 3, enumerable.error]
+    end
+  end
+end
---
+++ spec/reacto/trackable/class_level/value_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+context Reacto::Trackable do
+  context '.value' do
+    it 'emits only the passed value and then closes' do
+      trackable = described_class.value(5)
+      subscription = attach_test_trackers(trackable)
+
+      expect(test_data).to be == [5, '|']
+    end
+  end
+end
